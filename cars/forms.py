4402b2a20e503af12dec9f1d380b165cc2ed7f67
Shreyas Iyengar <32004803+pinkslp@users.noreply.github.com>
2020-12-01 17:38:48 -0500

    added search

---
+++ cars/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from cars.models import Manufacturers
+
+class ManuForm(forms.ModelForm):
+    class Meta:
+        model = Manufacturers
+        fields = ('name', 'country', 'year_founded', 'revenue',)
