560e254edfbc723973f101f34d77f0e9eb2b2429
Bharat Iyer <bhiyer9@gmail.com>
2020-12-03 21:51:39 -0800

    pushin this change to the branch

--- cars/views.py
+++ cars/views.py
@@ -1,9 +1,7 @@
-
-
-# Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
 from cars.forms import ManuForm
+from django.db import connection
 
 
 from .models import Manufacturers
@@ -15,6 +13,16 @@ def index(request):
 def manu(request):
     manufacturers = Manufacturers.objects.all()
     return render(request, "Manufacturers.html", {'manus': manufacturers})
+
+def add_Manu(request):
+    cursor = connection.cursor()
+    name = request.POST["name"]
+    country = request.POST["country"]
+    year = name = request.POST["year_founded"]
+    revenue = request.POST["revenue"]
+    cursor.execute("""INSERT INTO cars_Manufacturers (name, country, year_founded,revenue) VALUES (%s,%s,%s,%s)""",(name, country, year, revenue))
+    return render(request, "add_manufactuers.html")
+
 
 def home(request):
     return render(request, "home.html", {})
